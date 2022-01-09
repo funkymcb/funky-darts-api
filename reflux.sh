6de3b528066b31ba423e04338379cd5a2a3b55f7
Yannick Peter <yannick.peter@fino.digital>
2022-01-09 14:27:48 +0100

    fix reflux script

--- reflux.sh
+++ reflux.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+create_release() {
+    OUTPUT_FILE=$(mktemp)
+    HTTP_CODE=$(
+        curl -H "X-Authorization-Nonce: $NONCE" \
+            -H "X-Authorization-Timestamp: $TIMESTAMP" \
+            -H "X-Authorization-Content-Sha256: $HMAC_HASH" \
+            -o "$OUTPUT_FILE" -w "%{http_code}" \
+            -s -d "$BODY" "$URI"
+    )
+    if [[ ${HTTP_CODE} -lt 200 || ${HTTP_CODE} -gt 299 ]] ; then
+        >&2 cat "$OUTPUT_FILE"
+        exit 1
+    fi
+    cat "$OUTPUT_FILE"
+    rm "$OUTPUT_FILE"
+}
+
 # post data
 NAMESPACE="flux-system"
 WORKLOAD="funky-darts-api"
@@ -32,7 +49,4 @@ MESSAGE=$(printf "%s-%s-%s-%s" "$NONCE" "$TIMESTAMP" "$URI" "$BODY")
 HMAC_HASH=$(printf %s "$MESSAGE" | openssl dgst -sha256 -hmac "$SECRET" | sed 's/^.*= //')
 
 # update image policy
-curl -v -H "X-Authorization-Nonce: $NONCE" \
-    -H "X-Authorization-Timestamp: $TIMESTAMP" \
-    -H "X-Authorization-Content-Sha256: $HMAC_HASH" \
-    -d "$BODY" "$URI"
+create_release
