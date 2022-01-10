8296b58fb29089b50523ce76869a06ecac61ac3c
Yannick Peter <yannick.peter@fino.digital>
2022-01-10 07:47:25 +0100

    test new imagepol creation endpoint of reflux

--- reflux.sh
+++ reflux.sh
@@ -12,20 +12,38 @@ create_release() {
             -s -d "$BODY" "$URI"
     )
     if [[ ${HTTP_CODE} -lt 200 || ${HTTP_CODE} -gt 299 ]] ; then
-        >&2 cat "$OUTPUT_FILE"
-        exit 1
+        ERROR_NOT_FOUND="not found"
+        if [[ $(cat "$OUTPUT_FILE") =~ .*"$ERROR_NOT_FOUND".* ]]; then
+            create_imagepolicy
+        fi
     fi
     cat "$OUTPUT_FILE"
     rm "$OUTPUT_FILE"
+}
+
+create_imagepolicy() {
+    IMAGEPOL_OUTPUT_FILE=$(mktemp)
+    HTTP_CODE=$(
+        curl -H "X-Authorization-Nonce: $NONCE" \
+            -H "X-Authorization-Timestamp: $TIMESTAMP" \
+            -H "X-Authorization-Content-Sha256: $HMAC_HASH" \
+            -o "$OUTPUT_FILE" -w "%{http_code}" \
+            -s -d "$BODY" "$URI"
+    )
+    if [[ ${HTTP_CODE} -lt 200 || ${HTTP_CODE} -gt 299 ]] ; then
+        >&2 cat "$IMAGEPOL_OUTPUT_FILE"
+        exit 1
+    fi
+    cat "$IMAGEPOL_OUTPUT_FILE"
+    rm "$IMAGEPOL_OUTPUT_FILE"
 }
 
 # post data
 NAMESPACE="flux-system"
 WORKLOAD="funky-darts-api"
 KIND="deployment"
-URI="https://reflux.funkyd.art/release"
 
-while getopts ":n:s:t:" opt; do
+while getopts ":e:n:s:t:" opt; do
     case $opt in
         n)
             NONCE=$OPTARG
@@ -36,8 +54,24 @@ while getopts ":n:s:t:" opt; do
         t)
             VERSION=$OPTARG
             ;;
+        e)
+            case $OPTARG in
+                'dev')
+                    URI="https://reflux.dev.funkyd.art/release"
+                    ;;
+                'test')
+                    URI="https://reflux.test.funkyd.art/release"
+                    ;;
+                'prod')
+                    URI="https://reflux.funkyd.art/release"
+                    ;;
+                *)
+                    echo "valid environments are [dev, test, prod]"
+                    ;;
+            esac
+            ;;
         *)
-            echo "-n -s and -t flags are required"
+            echo "-e -n -s and -t flags are required"
             ;;
     esac
 done
